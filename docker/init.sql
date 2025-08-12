daf6e07ee33bed361fe91f23fb605ccee5186eba
NguyenLam1708 <lam170804@gmail.com>
2025-08-12 15:36:27 +0700

    feat : add init sql

---
+++ docker/init.sql
@@ -0,0 +1,74 @@
+-- Tạo ENUMs
+CREATE TYPE user_role AS ENUM ('user', 'admin');
+CREATE TYPE user_status AS ENUM ('pending', 'active', 'banned');
+CREATE TYPE order_status AS ENUM ('pending', 'confirmed', 'shipped', 'delivered', 'cancelled');
+
+-- Bảng users
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL ,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone VARCHAR(20),
+    address TEXT,
+    role user_role DEFAULT 'user',
+    status user_status DEFAULT 'pending',
+    otp_code VARCHAR(6),
+    otp_expires_at TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-- Bảng categories
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Bảng products
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL,
+    quantity INT DEFAULT 0,
+    image_url VARCHAR(255),
+    category_id INT REFERENCES categories(id) ON DELETE CASCADE,
+    size VARCHAR(10),
+    color VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Bảng cart
+CREATE TABLE cart (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    product_id INT REFERENCES products(id) ON DELETE CASCADE,
+    quantity INT NOT NULL DEFAULT 1,
+    added_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Bảng orders
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    total_amount DECIMAL(10, 2) NOT NULL,
+    shipping_address TEXT NOT NULL,
+    status order_status DEFAULT 'pending',
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Bảng order_items
+CREATE TABLE order_items (
+    id SERIAL PRIMARY KEY,
+    order_id INT REFERENCES orders(id) ON DELETE CASCADE,
+    product_id INT REFERENCES products(id) ON DELETE CASCADE,
+    quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL
+);
+
+-- Thêm dữ liệu mẫu
+INSERT INTO categories (name) VALUES ('Glasses'), ('Sunglasses');
+INSERT INTO users (name, email, password, role)
+VALUES ('Admin', 'admin@example.com', '123456', 'admin');
