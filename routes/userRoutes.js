5cf70c9b34f71e4a0954cc23209602c607a0a666
NguyenLam1708 <lam170804@gmail.com>
2025-08-13 20:53:02 +0700

    feat: add routes for forgotPassword,getAllUsers

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,5 +6,7 @@ const authMiddleware = require("../middleware/authMiddleware")
 router.get("/me", authMiddleware, userController.getMe);
 router.post("/change-password", authMiddleware, userController.changePassword);
 router.post("/change-info", authMiddleware, userController.changeInfo);
+router.post("/forgot-password", authMiddleware, userController.forgotPassword);
+router.get("",authMiddleware("admin"),userController.getAllUsers);
 
 module.exports = router;
