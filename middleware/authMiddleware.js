b381cdd2825630f7af80861b8693b08be45f1b6d
NguyenLam1708 <lam170804@gmail.com>
2025-08-13 20:52:30 +0700

    refactor: add check role

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,20 +1,28 @@
 const jwt = require("jsonwebtoken");
-require('dotenv').config();
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const auth = (roles = []) => {
+  const allowedRoles = typeof roles === 'string' ? [roles] : roles;
 
-module.exports = (req, res, next) => {
-    const header = req.header('Authorization') || req.header("authorization");
-    if(!header || !header.startsWith('Bearer ')){
-        return res.status(401).json({message:"Không có token"});
+  return (req, res, next) => {
+    const header = req.header('Authorization') || req.header('authorization');
+    if (!header || !header.startsWith('Bearer ')) {
+      return res.status(401).json({ message: 'Không có token' });
     }
 
     const token = header.split(' ')[1];
     try {
-        const decoded = jwt .verify(token,JWT_SECRET);
-        req.user = decoded;
-        return next();
-    } catch (err) {
-        return res.status(401).json({message: 'Token không hợp lệ'});
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+
+      if (allowedRoles.length && !allowedRoles.includes(decoded.role)) {
+        return res.status(403).json({ message: 'Bạn không có quyền truy cập' });
+      }
+
+      next();
+    } catch {
+      return res.status(401).json({ message: 'Token không hợp lệ' });
     }
+  };
 };
+
+module.exports = auth;
