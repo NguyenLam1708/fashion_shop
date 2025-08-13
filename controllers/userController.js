ebb74821cb9a3601d5f41828806e55c454ec3d66
NguyenLam1708 <lam170804@gmail.com>
2025-08-13 20:51:45 +0700

    feat : add forgotPassword, getAllUsers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
-
+const nodemailer = require("nodemailer");
+const { Op } = require("sequelize");
 //user
 // lấy thông tin người dùng
 exports.getMe = async (req, res) => {
@@ -76,13 +77,11 @@ exports.changeInfo = async (req,res) =>{
         if(!user) return res.status(404).json({message:"Người dùng không tồn tại"});
 
         const { name, address, phone} = req.body;
-        User.update({
+        user.update({
             name: name || user.name,
             address: address || user.address,
             phone: phone || user.phone
-        },
-        { where: { id: userId }}
-        );
+        });
 
         return res.status(200).json({
             message: "Đổi thông tin người dùng thành công",
@@ -98,7 +97,71 @@ exports.changeInfo = async (req,res) =>{
     };
 };
 // quên mật khẩu 
+exports.forgotPassword = async(req,res)=>{
+    try{
+        const {email} =  req.body;
+        const user = await User.findOne({where:{email}});
+        if(!user) return res.status(404).json({message:"Email không tồn tại"});
 
+        const transporter = nodemailer.createTransport({
+            service:"gmail",
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS
+            }
+        })
+        const newPassword = Math.random().toString(36).slice(-6);
+        await transporter.sendMail(
+            {
+                from: process.env.MAIL_USER,
+                to:email,
+                subject:"Fashion_shop gửi bạn mật khẩu mới",
+                text:`Mật khẩu mới của bạn là: ${newPassword}`
+            }
+        )
+        const hashedPassword = await bcrypt.hash(newPassword,10);
+        user.update({
+            password: hashedPassword
+            }
+        );
+        return res.status(200).json({message:"Vui lòng kiểm tra Gmail để nhận mật khẩu mới"})
+    }catch(error){
+        console.error(error)
+        return res.status(500).json({message:"Lỗi server",error: error.message})
+    }
+};
 //admin
-// lấy tất cả người dùng, phân trang 
+// lấy tất cả người dùng, phân trang
+exports.getAllUsers = async (req,res) =>{
+    try{
+        let { page = 1, limit = 10 } = req.query;
+        page = parseInt(page);
+        limit = parseInt(limit);
+
+        //Tính offset
+        const offset = (page-1) * limit;
+
+        const {count, rows} = await User.findAndCountAll({
+            attributes:["id","name","email","role","phone","address","status","createdAt"],
+            limit,
+            offset,
+            order: [["createdAt", "DESC"]],
+            where: { role: { [Op.ne]: "admin" } }, // Op = not equal
+        })
+         // Trả về kết quả
+        return res.status(200).json({
+            message: "Lấy danh sách người dùng thành công",
+            data: {
+                totalUsers: count,
+                totalPages: Math.ceil(count / limit),
+                currentPage: page,
+                users: rows
+            }
+        });
+    }catch(error){
+        console.error(error)
+        res.status(500).json({message:"Lỗi server",error: error.message})
+    }
+} 
+// chỉnh sửa người dùng
 // ban người dùng
