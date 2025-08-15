49ea193f6b42d601a8a482a8a92ffe672a142185
NguyenLam1708 <lam170804@gmail.com>
2025-08-15 17:51:29 +0700

    feat: check authorization and add getUserById

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const authMiddleware = require("../middleware/authMiddleware")
 //user
-router.get("/me", authMiddleware, userController.getMe);
-router.post("/change-password", authMiddleware, userController.changePassword);
-router.post("/change-info", authMiddleware, userController.changeInfo);
-router.post("/forgot-password", authMiddleware, userController.forgotPassword);
+router.get("/my-info", authMiddleware("user"), userController.getMe);
+router.post("/change-password", authMiddleware(), userController.changePassword);
+router.post("/change-info", authMiddleware(), userController.changeInfo);
+router.post("/forgot-password", authMiddleware(), userController.forgotPassword);
 
 //admin
 router.get("",authMiddleware("admin"),userController.getAllUsers);
 router.put("/:id", authMiddleware("admin"), userController.updateUser);
+router.get("/:id", authMiddleware("admin"), userController.getUserById);
 
 module.exports = router;
