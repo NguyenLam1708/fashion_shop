a0da23b7df839098459292dec951594050746f27
NguyenLam1708 <lam170804@gmail.com>
2025-08-22 17:05:58 +0700

    feat: add route for product

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,6 +4,11 @@ const authMiddleware = require("../middleware/authMiddleware");
 const productController = require("../controllers/productController");
 const upload = require('../middleware/upload');
 
-router.post("",authMiddleware("admin"), upload.array('images', 5), productController.createProduct);
+router.get("/", authMiddleware(), productController.getAllProducts);
+router.get("/category/:categoryId", authMiddleware(), productController.getProductByCategory);
+router.get("/:id", authMiddleware(), productController.getProduct);
+router.post("/", authMiddleware("admin"), upload.array('images', 5), productController.createProduct);
+router.delete("/:id", authMiddleware("admin"), productController.deleteProduct);
+router.put("/:id", authMiddleware("admin"), upload.array("images", 5),productController.updateProduct);
 
 module.exports = router;
