05ffb9df29a81fd8c94580cb964c108fe8b25a15
NguyenLam1708 <lam170804@gmail.com>
2025-08-26 23:20:22 +0700

    refactor(user): drop authMiddleware for forgot-password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 const authMiddleware = require("../middleware/authMiddleware")
-//user
-router.get("/my-info", authMiddleware("user"), userController.getMe);
+
+router.get("/my-info", authMiddleware(), userController.getMe);
 router.post("/change-password", authMiddleware(), userController.changePassword);
 router.post("/change-info", authMiddleware(), userController.changeInfo);
-router.post("/forgot-password", authMiddleware(), userController.forgotPassword);
+router.post("/forgot-password", userController.forgotPassword);
 
 //admin
 router.get("",authMiddleware("admin"),userController.getAllUsers);
